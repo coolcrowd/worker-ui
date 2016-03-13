4c8c2d29efda0a54f5745d6f50e3577c1b86acef
Simon Korz <zeit.vertreib@web.de>
2016-03-13 23:58:25 +0100

    Add button in debug.html to reset worker, fix #17

--- src/js/app.js
+++ src/js/app.js
@@ -150,6 +150,12 @@ WorkerUI = (function () {
         return NO_AUTH;
     }
 
+    function clearAuthorization() {
+        if (typeof(Storage) !== "undefined") {
+            sessionStorage.clear();
+        }
+    }
+
     function getAuthenticationHeader() {
         var headers = {};
         if (jwt !== NO_AUTH) {
@@ -655,6 +661,10 @@ WorkerUI = (function () {
 
         beforeIdentifyWorker: function (call) {
             hooks.identifyWorker = call;
+        },
+
+        clearWorker: function () {
+            clearAuthorization();
         },
 
 
